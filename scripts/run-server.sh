860095030dd399f7ecd2dffab9f127897d1b81bb
Charles Lgn <charles.ligony@gmail.com>
2025-08-16 13:05:26 +0200

    remove scheduling
    add a linux profile for log
    
    add latest.log on root is not on linux profile

--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-screen -dmS rvc-server -L -Logfile latest.log ./mvnw spring-boot:run -pl "app"
+# TODO - remove de dev profile once postgres is setup
+screen -dmS rvc-server -L -Logfile latest.log ./mvnw spring-boot:run -pl "app" -Dspring-boot.run.profiles=dev,linux
