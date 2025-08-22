4e27452a99ad2675ba855cd410d3571d367d90c8
charles ligony <c-ligony@efluid.fr>
2025-08-22 15:40:23 +0200

    refactor(quarkus): migration

--- app/src/main/java/fr/revoicechat/web/UserControllerImpl.java
+++ app/src/main/java/fr/revoicechat/web/UserControllerImpl.java
@@ -2,14 +2,11 @@ package fr.revoicechat.web;
 
 import java.util.UUID;
 
-import org.springframework.web.bind.annotation.RestController;
-
 import fr.revoicechat.representation.user.UpdatableUserData;
 import fr.revoicechat.representation.user.UserRepresentation;
 import fr.revoicechat.service.UserService;
 import fr.revoicechat.web.api.UserController;
 
-@RestController
 public class UserControllerImpl implements UserController {
   private final UserService userService;
 
@@ -21,12 +18,7 @@ public class UserControllerImpl implements UserController {
   }
 
   @Override
-  public UserRepresentation me(final UpdatableUserData userData) {
-    return userService.updateConnectedUser(userData);
-  }
-
-  @Override
-  public UserRepresentation get(final UUID id) {
+  public UserRepresentation get(UUID id) {
     return userService.get(id);
   }
 }
