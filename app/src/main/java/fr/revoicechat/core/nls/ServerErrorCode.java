79b6df441a6124d95b6a8855e54d42bea7920ff0
charles ligony <c-ligony@efluid.fr>
2025-09-12 15:17:31 +0200

    add validation on server structure update

--- app/src/main/java/fr/revoicechat/core/nls/ServerErrorCode.java
+++ app/src/main/java/fr/revoicechat/core/nls/ServerErrorCode.java
@@ -2,7 +2,8 @@ package fr.revoicechat.core.nls;
 
 public enum ServerErrorCode implements LocalizedMessage {
   APPLICATION_DOES_NOT_ALLOW_SERVER_CREATION,
-  APPLICATION_DOES_NOT_ALLOW_SERVER_DELETION;
+  APPLICATION_DOES_NOT_ALLOW_SERVER_DELETION,
+  SERVER_STRUCTURE_WITH_ROOM_THAT_DOES_NOT_EXISTS;
 
   @Override
   public String toString() {
