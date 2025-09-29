76c47ca91ce1c7883a417a0d9ed9ac8dd2be5832
Charles Lgn <charles.ligony@gmail.com>
2025-09-29 16:08:52 +0200

    rename modulesÃ

--- security/src/test/java/fr/revoicechat/security/TestQueryParamJwtAuthMechanism.java
+++ security/src/test/java/fr/revoicechat/security/TestQueryParamJwtAuthMechanism.java
@@ -1,20 +1,23 @@
 package fr.revoicechat.security;
 
+import java.util.Set;
+import java.util.UUID;
+
 import org.junit.jupiter.api.Test;
 
-import fr.revoicechat.core.junit.CleanDatabase;
-import fr.revoicechat.core.quarkus.profile.MultiServerProfile;
-import fr.revoicechat.core.web.tests.RestTestUtils;
-import fr.revoicechat.security.TestQueryParamJwtAuthMechanism.DevTestProfile;
+import fr.revoicechat.security.model.AuthenticatedUser;
+import fr.revoicechat.security.service.SecurityTokenService;
 import io.quarkus.test.junit.QuarkusTest;
-import io.quarkus.test.junit.TestProfile;
 import io.restassured.RestAssured;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.core.MediaType;
 
 @QuarkusTest
-@CleanDatabase
-@TestProfile(DevTestProfile.class)
 class TestQueryParamJwtAuthMechanism {
+
+  private static final String ID_USER = "35117c82-3b6f-403f-be5a-f3ee842d97d6";
+
+  @Inject SecurityTokenService jwtService;
 
   @Test
   void testWrongJwt() {
@@ -38,7 +41,7 @@ class TestQueryParamJwtAuthMechanism {
 
   @Test
   void test() {
-    String validJwt = RestTestUtils.logNewUser();
+    var validJwt = jwtService.generate(new AuthenticatedUserMock());
     RestAssured.given()
                .accept(MediaType.APPLICATION_JSON)
                .contentType(MediaType.APPLICATION_JSON)
@@ -47,10 +50,26 @@ class TestQueryParamJwtAuthMechanism {
                .statusCode(200);
   }
 
-  public static class DevTestProfile extends MultiServerProfile {
+  private static class AuthenticatedUserMock implements AuthenticatedUser {
+
     @Override
-    public String getConfigProfile() {
-      return "dev,test";
+    public UUID getId() {
+      return UUID.fromString(ID_USER);
+    }
+
+    @Override
+    public String getDisplayName() {
+      return "user";
+    }
+
+    @Override
+    public String getLogin() {
+      return "user";
+    }
+
+    @Override
+    public Set<String> getRoles() {
+      return Set.of("USER");
     }
   }
 }
