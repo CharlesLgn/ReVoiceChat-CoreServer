1decc46dd88bb7067230ba018c5897210c5e3e71
Charles Lgn <charles.ligony@gmail.com>
2025-10-06 22:02:27 +0200

    refac risk management + reword 'risk' to 'authorization'

---
+++ app/src/main/java/fr/revoicechat/risk/type/InvitationRiskType.java
@@ -0,0 +1,7 @@
+package fr.revoicechat.risk.type;
+
+@RiskCategory("INVITATION_RISK_TYPE")
+public enum InvitationRiskType implements RiskType {
+  SERVER_INVITATION_ADD,
+  SERVER_INVITATION_FETCH,
+}
