cd7392e3abf609fb2df674a457e3b6c8c70cd383
Charles Lgn <charles.ligony@gmail.com>
2025-10-10 23:31:57 +0200

    add file type support

--- app/src/main/java/fr/revoicechat/core/model/FileType.java
+++ app/src/main/java/fr/revoicechat/core/model/FileType.java
@@ -1,9 +1,9 @@
 package fr.revoicechat.core.model;
 
 public enum FileType {
-  PICTURE,
-  SVG,
-  VIDEO,
-  PDF,
-  OTHER
+  PICTURE, SVG,     VIDEO,
+  AUDIO,   PDF,     TEXT,
+  OFFICE,  ARCHIVE, CODE,
+  FONT,    MODEL,   EXECUTABLE,
+  DATA,    OTHER
 }
