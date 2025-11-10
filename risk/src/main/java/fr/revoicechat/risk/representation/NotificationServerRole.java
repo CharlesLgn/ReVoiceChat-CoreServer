03f0c941cbd550ed07ae50bb62d7d75663232b44
charles ligony <c-ligony@efluid.fr>
2025-11-10 13:30:20 +0100

    add represation to risk update notification

--- risk/src/main/java/fr/revoicechat/risk/representation/NotificationServerRole.java
+++ risk/src/main/java/fr/revoicechat/risk/representation/NotificationServerRole.java
@@ -4,7 +4,8 @@ import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import fr.revoicechat.notification.model.NotificationPayload;
 import fr.revoicechat.notification.model.NotificationType;
+import fr.revoicechat.notification.representation.NotificationActionType;
 
 @NotificationType(name = "RISK_MANAGEMENT")
 @Schema(description = "risk management notification")
-public record NotificationServerRole() implements NotificationPayload {}
+public record NotificationServerRole(ServerRoleRepresentation role, NotificationActionType action) implements NotificationPayload {}
