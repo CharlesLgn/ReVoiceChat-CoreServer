386b7d58d381d28242ea53ad3586037029253cb4
Charles Lgn <charles.ligony@gmail.com>
2025-11-29 14:51:34 +0100

    feat(media): permit all

--- app/src/main/java/fr/revoicechat/core/web/MediaDataControllerImpl.java
+++ app/src/main/java/fr/revoicechat/core/web/MediaDataControllerImpl.java
@@ -5,6 +5,7 @@ import static fr.revoicechat.security.utils.RevoiceChatRoles.ROLE_USER;
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.annotation.security.PermitAll;
 import jakarta.annotation.security.RolesAllowed;
 
 import fr.revoicechat.core.model.MediaDataStatus;
@@ -13,7 +14,6 @@ import fr.revoicechat.core.representation.media.UpdatableMediaDataStatus;
 import fr.revoicechat.core.service.media.MediaDataService;
 import fr.revoicechat.core.web.api.MediaDataController;
 
-@RolesAllowed(ROLE_USER)
 public class MediaDataControllerImpl implements MediaDataController {
 
   private final MediaDataService mediaDataService;
@@ -23,21 +23,25 @@ public class MediaDataControllerImpl implements MediaDataController {
   }
 
   @Override
+  @PermitAll
   public MediaDataRepresentation get(final UUID id) {
     return mediaDataService.get(id);
   }
 
   @Override
+  @RolesAllowed(ROLE_USER)
   public MediaDataRepresentation updateMediaByStatus(final UUID id, final UpdatableMediaDataStatus status) {
     return mediaDataService.update(id, MediaDataStatus.valueOf(status.name()));
   }
 
   @Override
+  @RolesAllowed(ROLE_USER)
   public List<MediaDataRepresentation> findMediaByStatus(final MediaDataStatus status) {
     return mediaDataService.findMediaByStatus(status);
   }
 
   @Override
+  @RolesAllowed(ROLE_USER)
   public MediaDataRepresentation delete(final UUID id) {
     return mediaDataService.update(id, MediaDataStatus.DELETING);
   }
